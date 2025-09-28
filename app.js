8e5d7cc141a89210b3129ba119a294cfc8a4a007
Tabrez <metabrez@gmail.com>
2025-09-27 23:02:28 -0400

    added search

--- app.js
+++ app.js
@@ -2,22 +2,28 @@ const input = document.getElementById("taskInput");
 console.log(input);
 const add = document.getElementById("addBtn");
 console.log(add);
-const list = document.getElementById("taskList");
+const taskList = document.getElementById("taskList");
+const taskContainer = document.getElementById("task");
 
 const searchItem = document.getElementById("searchInput");
 
+// load task from LocalStorage on page load.
 document.addEventListener("DOMContentLoaded", loadTasks);
 
-function saveTasks() {
-  const tasks = [];
-  const items = list.getElementsByTagName("li");
+function saveTasksToLocalStorage() {
+  const taskItems = document.querySelectorAll("#taskList, li");
+  const taskValues = [];
+  console.log(taskItems);
+  const task = taskItems.forEach((item) => {
+    const span = item.querySelector("span");
+    if (span) {
+      taskValues.push(span.textContent.trim());
+    }
+  });
 
-  for (let i = 0; i < items.length; i++) {
-    const span = items[i].getElementsByTagName("span")[0];
-    tasks.push(span.textContent);
-  }
-  localStorage.setItem("tasks", JSON.stringify(tasks));
-  //localStorage.setItem("tasks", tasks);
+  localStorage.setItem("tasks", JSON.stringify(taskValues));
+
+  console.log(taskValues);
 }
 
 function loadTasks() {
@@ -25,83 +31,71 @@ function loadTasks() {
   tasks.forEach((task) => addTaskToList(task));
 }
 
-// add task
-
 function addTaskToList(val) {
   const li = document.createElement("li");
-  //li.textContent = val;
-
-  // create a span
   const span = document.createElement("span");
+
   span.textContent = val;
-  //span.style.marginLeft = 10;
-  span.style.marginLeft = 15;
 
   // create edit button
   const edit = document.createElement("button");
   edit.textContent = "edit";
-  edit.style.color = "blue";
   edit.style.marginLeft = "10px";
 
   // create a delete button
-  const deleteBtn = document.createElement("button");
-  deleteBtn.textContent = "delete";
-  deleteBtn.style.marginLeft = "10px";
+  const del = document.createElement("button");
+  del.textContent = "delete";
+  del.style.marginLeft = "10px";
 
   // edit functionality
-  edit.addEventListener("click", () => {
-    const newVal = prompt("Edit Task: ", span.textContent);
-    if (newVal !== null && newVal.trim() !== "") {
-      span.textContent = newVal.trim();
-      saveTasks();
-    }
 
-    // list.appendChild(li);
+  edit.addEventListener("click", () => {
+    const listVal = prompt("edit task:", span.textContent);
+    if (listVal !== null && listVal !== "") {
+      span.textContent = listVal;
+    }
+    saveTasksToLocalStorage();
   });
 
   // delete functionality
-  deleteBtn.addEventListener("click", () => {
-    list.removeChild(li);
-    saveTasks();
+  del.addEventListener("click", () => {
+    taskList.removeChild(li);
+    saveTasksToLocalStorage();
   });
 
   li.appendChild(span);
+
   li.appendChild(edit);
-  li.appendChild(deleteBtn);
-  list.appendChild(li);
-  input.value = "";
+
+  li.appendChild(del);
+
+  taskList.appendChild(li);
+
+  taskContainer.appendChild(taskList);
 }
 
 add.addEventListener("click", (e) => {
   e.preventDefault();
-
-  const val = input.value.trim();
-  if (val === null || val === "") {
-    window.alert("please enter task");
+  val = input.value.trim();
+  if (!val) {
+    window.alert("Please enter task");
     return;
   }
 
   addTaskToList(val);
-  saveTasks(); // update localstorage
+  saveTasksToLocalStorage();
+
   input.value = "";
 });
 
-// search functionality
-searchItem.addEventListener("keyup", () => {
-  const filter = searchItem.value.toLowerCase();
-  const items = list.getElementsByTagName("li");
-  console.log(items);
+searchItem.addEventListener("input", () => {
+  const search = searchItem.value.trim().toLowerCase();
 
-  for (let i = 0; i < items.length; i++) {
-    const span = items[i].getElementsByTagName("span")[0];
-    console.log(span);
-    const text = span.textContent.toLowerCase();
+  const taskListItem = document.querySelectorAll("#taskList li");
+  console.log(taskListItem);
+  taskListItem.forEach((item) => {
+    const text = item.querySelector("span").textContent.toLowerCase();
     console.log(text);
-
-    if (text.includes(filter)) {
-      items[i].style.display = "";
-    } else {
-      items[i].style.display = "none";
-    }
-  }
+    item.style.display = text.includes(search) ? "" : "none";
+  });
 });
